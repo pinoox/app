c437a080aa7eda45ddc0fe89a25185428d4530c7
yoosefap <yoosefalipour@gmail.com>
2026-06-13 20:36:40 +0330

    refactor: simplify named action routes with Action helper

--- Router/Actions.php
+++ Router/Actions.php
@@ -2,7 +2,23 @@
 
 namespace App\com_pinoox_app\Router;
 
+/**
+ * Named route action identifiers for this app.
+ *
+ * Use with routes:  get('/')->actionName(Actions::HOME)
+ * Register in:      routes/actions.php via action(Actions::HOME, ...)
+ */
 final class Actions
 {
     public const HOME = 'home';
+
+    /**
+     * @return list<string>
+     */
+    public static function all(): array
+    {
+        return [
+            self::HOME,
+        ];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
-<?php
-
-use App\com_pinoox_app\Router\Actions;
-use function Pinoox\Router\get;
-
-get('/', '@' . Actions::HOME);
+<?php
+
+use App\com_pinoox_app\Router\Actions;
+use function Pinoox\Router\get;
+
+get('/')->actionName(Actions::HOME);
